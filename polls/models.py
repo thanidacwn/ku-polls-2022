980e84eb25f9fb2d62ea211f962c4e0913b5717a
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2022-09-11 12:50:13 +0700

    add two method

--- polls/models.py
+++ polls/models.py
@@ -17,6 +17,15 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    def is_published(self):
+        return timezone.localtime() >= self.pub_date
+
+    def can_vote(self):
+        if self.end_date:
+            return self.pub_date >= timezone.localtime() >= self.end_date
+        return self.is_published()
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
