885a673b3c635039ecbb1cdc777336d7af0a4028
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2022-09-18 12:39:00 +0700

    fix runserver

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY', cast=str, default="miss secret key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
